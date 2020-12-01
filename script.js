cad37ae03ca948f321fc85e61babe755efdc4bdd
Daniel Woldehana <danielwoldehana@MacBook-Air.local>
2020-12-01 09:57:14 -0500

    first Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let x = 5;
+let y = 23;
+let z = x + y;
+
+let myP = document.querySelector("p");
+
+// let myBody = documnet.querySelector(".demo");
+
+
+
+function myVery() {
+    myP.setAttribute("style", "color: purple;")
+};
